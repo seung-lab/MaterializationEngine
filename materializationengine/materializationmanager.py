2a46e50568bd0b9bc0934b6b1c47575bebb35bb3
sdorkenw <sven.dorkenwald@googlemail.com>
2018-07-26 15:01:25 -0700

    rewrote materialization and added MaterializationManager class

---
+++ materializationengine/materializationmanager.py
@@ -0,0 +1,155 @@
+import json
+import functools
+
+from emannotationschemas.models import make_annotation_model, Base
+from emannotationschemas.base import flatten_dict
+from emannotationschemas import get_schema
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class MaterializeAnnotationException(Exception):
+    pass
+
+
+class RootIDNotFoundException(MaterializeAnnotationException):
+    pass
+
+
+class AnnotationParseFailure(MaterializeAnnotationException):
+    pass
+
+
+def materialize_bsp(sv_id_to_root_id_dict, item):
+    """ Function to fill in root_id of BoundSpatialPoint fields
+    using the dictionary provided. Will alter item in place.
+    Meant to be passed to mm.Schema as a context variable ('bsp_fn')
+    :param sv_id_to_root_id_dict: dict
+        dictionary to look up root ids from supervoxel id
+    :param item: dict
+        deserialized boundspatialpoint to process
+    :return: None
+        will edit item in place
+    """
+
+    try:
+        item['root_id'] = sv_id_to_root_id_dict[item['supervoxel_id']]
+    except KeyError:
+        msg = "cannot process {}, root_id not found in {}"
+        raise RootIDNotFoundException(msg.format(item, sv_id_to_root_id_dict))
+
+
+class MaterializationManager(object):
+    def __init__(self, dataset_name, annotation_type,
+                 sqlalchemy_database_uri=None):
+        self._dataset_name = dataset_name
+        self._annotation_type = annotation_type
+        self._sqlalchemy_database_uri = sqlalchemy_database_uri
+
+        self._annotation_model = make_annotation_model(dataset_name,
+                                                       annotation_type)
+
+        if sqlalchemy_database_uri is not None:
+            self._sqlalchemy_engine = create_engine(sqlalchemy_database_uri,
+                                                    echo=True)
+            Base.metadata.create_all(self.sqlalchemy_engine)
+
+            self._sqlalchemy_session = sessionmaker(bind=self.sqlalchemy_engine)
+        else:
+            self._sqlalchemy_engine = None
+            self._sqlalchemy_session = None
+
+        self._schema_init = get_schema(self.annotation_type)
+
+
+    @property
+    def annotation_type(self):
+        return self._annotation_type
+
+    @property
+    def dataset_name(self):
+        return self._dataset_name
+
+    @property
+    def sqlalchemy_database_uri(self):
+        return self._sqlalchemy_database_uri
+
+    @property
+    def sqlalchemy_engine(self):
+        return self._sqlalchemy_engine
+
+    @property
+    def sqlalchemy_session(self):
+        return self._sqlalchemy_session
+
+    @property
+    def annotation_model(self):
+        return self._annotation_model
+
+    @property
+    def is_sql(self):
+        return not self.sqlalchemy_database_uri is None
+
+    @property
+    def schema_init(self):
+        return self._schema_init
+
+    def get_serialized_info(self):
+        """ Puts all initialization parameters into a dict
+
+        :return: dict
+        """
+        info = {"dataset_name": self.dataset_name,
+                "annotation_type": self.annotation_type,
+                "sqlalchemy_database_uri": self.sqlalchemy_database_uri}
+
+        return info
+
+    def get_schema(self, sv_id_to_root_id_dict):
+        """ Loads schema with appropriate context
+
+        :param root_id_d:
+        :return:
+        """
+        context = dict()
+        # context['bsp_fn'] = functools.partial(materialize_bsp,
+        #                                       sv_id_to_root_id_dict)
+        context['postgis'] = self.is_sql
+        return self.schema_init(context=context)
+
+    def deserialize_single_annotation(self, blob, sv_id_to_root_id_dict):
+        """ Materializes single annotation object
+
+        :param blob: binary
+        :param sv_id_to_root_id_dict: dict
+        :return: dict
+        """
+        schema = self.get_schema(sv_id_to_root_id_dict)
+        data = schema.load(json.loads(blob)).data
+
+        return flatten_dict(data)
+
+    def add_annotation_to_sql_database(self, deserialized_annotation):
+        """ Transforms annotation object into postgis format and commits to the
+            database
+
+        :param deserialized_annotation: dict
+        """
+        assert self.is_sql
+
+        # remove the type field because we don't want it as a column
+        deserialized_annotation.pop('type', None)
+
+        # # create a new model instance with data
+        annotation = self.annotation_model(**deserialized_annotation)
+
+        print(annotation)
+
+        # create a new db session
+        this_session = self.sqlalchemy_session()
+
+        # add this annotation object to database
+        this_session.add(annotation)
+
+        # commit this transaction to database
+        this_session.commit()
