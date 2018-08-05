ce144e77da0037496ce2ff60ed69c8bd56dd9b63
sdorkenw <sven.dorkenwald@googlemail.com>
2018-08-06 01:40:14 +0200

    improved synapse example

--- synapse_example.py
+++ synapse_example.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import os
 import numpy as np
+import time
 
-# from annotationengine import annotation, annotationclient
+from annotationengine import annotation, annotationclient
 
 HOME = os.path.expanduser("~")
 
@@ -12,10 +13,11 @@ def load_synapses(path=HOME + "/Downloads/pinky40_run2_remapped.df"):
 
     df = pd.read_csv(path)
 
-    sv_ids = np.array(df[["presyn_segid", "postsyn_segid"]])
+    locs = np.array(df[["presyn_x", "centroid_x", "postsyn_x"]])
     data = np.array(df)
 
-    mask = ~np.any(np.isnan(sv_ids), axis=1)
+    mask = ~np.any(np.isnan(locs), axis=1)
+
     df = df[mask]
 
     df['pre_pt.position'] = list(np.array(df[['presyn_x', 'presyn_y', 'presyn_z']], dtype=np.int))
@@ -25,14 +27,20 @@ def load_synapses(path=HOME + "/Downloads/pinky40_run2_remapped.df"):
     return df
 
 
-def insert_synapses(syn_df, dataset_name='pinky', annotation_type="synapse"):
-    ac = annotationclient.AnnotationClient("http://104.196.202.90:4000")
+def insert_synapses(syn_df, dataset_name='pinky40', annotation_type="synapse"):
+    ac = annotationclient.AnnotationClient("https://35.196.20.36:4001")
     ac.bulk_import_df(dataset_name, annotation_type, syn_df)
 
 
 if __name__ == "__main__":
 
     print("LOADING synapses")
-    syn_df = load_synapses()
 
-    syn_df = syn_df[:10]
+    time_start = time.time()
+    syn_df = load_synapses()
+    print("Time for loading: %.2fmin" % ((time.time() - time_start) / 60))
+
+    time_start = time.time()
+    insert_synapses(syn_df)
+    print("Time for inserting: %.2fmin" % ((time.time() - time_start) / 60))
+
