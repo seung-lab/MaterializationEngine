7a71823e0c318d7a9cc4d43bc388727215b6e4c7
Forrest Collman <forrest.collman@gmail.com>
2018-10-25 20:49:08 -0700

    some fixes towards tests

--- test/test_data.py
+++ test/test_data.py
@@ -2,9 +2,13 @@ import numpy as np
 from math import inf
 import json
 import pytest
+
+from pychunkedgraph.backend import chunkedgraph
+
 from materializationengine.materialize import materialize_all_annotations
 from annotationengine.annotation import collect_bound_spatial_points, import_annotation_func, get_schema_from_service
 from emannotationschemas.blueprint_app import get_type_schema
+from mock import patch, Mock, MagicMock
 
 
 @pytest.fixture(scope="session")
@@ -24,7 +28,7 @@ def test_data(test_annon_dataset):
     annotation_type = 'synapse'
     table_name = 'synapse'
     user_id = 'test_user'
-    amdb, dataset_name  = test_annon_dataset
+    amdb, dataset_name = test_annon_dataset
     amdb.create_table(user_id, dataset_name, annotation_type, table_name)
 
     synapse_d = {
@@ -40,7 +44,8 @@ def test_data(test_annon_dataset):
         }
     }
     schema = get_type_schema(annotation_type)
-    oids = import_annotation_func(synapse_d, user_id, dataset_name, table_name, schema, annotation_type, amdb)
+    oids = import_annotation_func(
+        synapse_d, user_id, dataset_name, table_name, schema, annotation_type, amdb)
 
     # bouton_shape_annotation = {
     #     'target_id': int(oids[0]),
@@ -57,29 +62,42 @@ def test_data(test_annon_dataset):
     yield test_annon_dataset
 
 
-def test_simple_test(test_data, test_annon_dataset):
-    cgraph, table_id = test_data
+def simple_get_root(self, atomic_id):
+    if (atomic_id <= 3):
+        return "12341324131851234"
+    else:
+        return "1345813419234993222"
+
+
+def test_simple_test(test_annon_dataset, monkeypatch):
+
     amdb, dataset_name = test_annon_dataset
 
     print(amdb.get_existing_tables())
 
-    df = materialize_all_annotations(table_id,
+    monkeypatch.setattr('pychunkedgraph.backend.chunkedgraph.ChunkedGraph',
+                        MagicMock(chunkedgraph.ChunkedGraph))
+    monkeypatch.setattr(
+        'pychunkedgraph.backend.chunkedgraph.ChunkedGraph.get_root', simple_get_root)
+
+    df = materialize_all_annotations('cgtable',
                                      dataset_name=dataset_name,
                                      annotation_type="synapse",
+                                     table_name = "synapse",
+                                     version='v1',
                                      amdb_client=amdb.client,
                                      amdb_instance_id=amdb.instance_id,
-                                     cg_client=cgraph.client,
-                                     cg_instance_id=cgraph.instance_id,
+                                     cg_instance_id='cgraph_instance',
                                      n_threads=1)
     print(df)
 
-    df_bs = materialize_all_annotations(table_id,
-                                        dataset_name=dataset_name,
-                                        annotation_type="bouton_shape",
-                                        amdb_client=amdb.client,
-                                        amdb_instance_id=amdb.instance_id,
-                                        cg_client=cgraph.client,
-                                        cg_instance_id=cgraph.instance_id,
-                                        n_threads=1)
+    # df_bs = materialize_all_annotations(table_id,
+    #                                     dataset_name=dataset_name,
+    #                                     annotation_type="bouton_shape",
+    #                                     amdb_client=amdb.client,
+    #                                     amdb_instance_id=amdb.instance_id,
+    #                                     cg_client=cgraph.client,
+    #                                     cg_instance_id=cgraph.instance_id,
+    #                                     n_threads=1)
 
-    print(df_bs)
+    # print(df_bs)
