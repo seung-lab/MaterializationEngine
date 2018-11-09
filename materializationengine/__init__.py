b3e2a85286cb65dd208250116061d51934db58ff
Forrest Collman <forrest.collman@gmail.com>
2018-11-09 14:41:58 -0800

    wip

--- materializationengine/__init__.py
+++ materializationengine/__init__.py
@@ -10,7 +10,8 @@ def create_app(test_config=None):
     from .materialize_bp import bp as materialize_bp
     from .utils import get_instance_folder_path
     from .database import db
-    
+    from .materialize_database import db_session
+
     # Define the Flask Object
     app = Flask(__name__,
                 static_folder="../static",
@@ -27,6 +28,11 @@ def create_app(test_config=None):
         db.create_all()
         admin = setup_admin(app, db)
 
-    app.register_blueprint(materialize_bp)
+    def shutdown_session(exception=None):
+        db_session.remove()
 
+    app.register_blueprint(materialize_bp)
+    app.teardown_appcontext(shutdown_session)
     return app
+
+
