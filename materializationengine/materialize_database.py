cd20293d15d419ca5d930e19679c0ca2dd60621a
Forrest Collman <forrest.collman@gmail.com>
2018-11-10 09:43:26 -0800

    removing extra db

--- materializationengine/materialize_database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from emannotationschemas.models import Base
-engine = create_engine('postgresql://postgres:welcometothematrix@35.196.105.34/postgres', convert_unicode=True)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-Base.query = db_session.query_property()
