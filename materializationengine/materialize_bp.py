29bea7683a52421eda738fc69355492dd0f2ef13
Forrest Collman <forrest.collman@gmail.com>
2019-03-10 19:19:32 -0700

    Bump version: 0.1.1 → 0.1.2

--- materializationengine/materialize_bp.py
+++ materializationengine/materialize_bp.py
@@ -12,7 +12,7 @@ import pandas as pd
 from emannotationschemas.models import make_annotation_model, make_dataset_models, declare_annotation_model
 import requests
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 bp = Blueprint("materialize", __name__, url_prefix="/materialize")
 
 
