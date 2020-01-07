298e55ce8b61a41590f19c6e4b93bef69ce4a269
Derrick <dlbrittain@gmail.com>
2020-01-07 12:57:40 -0800

    add flask cli  and app start

--- run.py
+++
@@ -1,6 +0,0 @@
-from app.main import create_app
-
-application = create_app()
-
-if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=8000, debug=True)
--- run_flask.sh
+++ run_flask.sh
@@ -1,3 +1,3 @@
-export FLASK_APP=run.py
+export FLASK_APP=manage.py
 export FLASK_ENV=development
 flask run --port 7777
