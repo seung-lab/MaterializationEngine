7e56b95139f3e3e88a8b593cd36c984a6fe7c42b
Derrick <dlbrittain@gmail.com>
2020-01-13 12:14:13 -0800

    Restructed to use tiangolo  entrypoints

--- app/app/factories/flask_app.py
+++ app/app/factories/flask_app.py
@@ -4,20 +4,18 @@ __version__ = "0.1.2"
 def create_app(test_config=None):
     from flask_sqlalchemy import SQLAlchemy
     from flask import Flask
-    from celery import Celery
     from app.config import configure_app
     from app.admin import setup_admin
-    from app.api import api
-    from app.routes import views
-    from .utils import get_instance_folder_path
-    from app.database import db, Base
+    from app.blueprints.api import api
+    from app.blueprints.routes import views
+    from app.utils import get_instance_folder_path
+    from app.extensions import db, Base, celery
+    from app.factories.celery_app import create_celery
     import logging
-    from app import celery
-    from app.celery_app import create_celery
 
     # Define the Flask Object
     app = Flask(__name__,
-                static_folder="../static",
+                static_folder="./static",
                 instance_path=get_instance_folder_path(),
                 instance_relative_config=True,
                 template_folder="./templates")
@@ -40,5 +38,3 @@ def create_app(test_config=None):
 
     return app
     
-if __name__ == "__main__":
-    create_app.run(debug=True, host="0.0.0.0", port=80)
