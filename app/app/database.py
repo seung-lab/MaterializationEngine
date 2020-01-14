3f69a3cb216b7e163200467bf83d8a864c71e2ba
Derrick <dlbrittain@gmail.com>
2020-01-14 15:37:33 -0800

    remove depreciated  files

--- app/app/database.py
+++
@@ -1,7 +0,0 @@
-from flask import g, current_app
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
-
-db = SQLAlchemy(model_class=Base)
-
