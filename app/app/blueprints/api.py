447f6dd05b364ee43e6c052f37312f6f412d0be2
Derrick <dlbrittain@gmail.com>
2020-02-25 11:39:29 -0800

    Basic materialization workflow

--- app/app/blueprints/api.py
+++ app/app/blueprints/api.py
@@ -2,9 +2,9 @@ from flask import Blueprint, jsonify, abort, current_app, request, render_templa
 from emannotationschemas.models import AnalysisTable, AnalysisVersion
 from app.schemas import AnalysisVersionSchema, AnalysisTableSchema, MaterializationSchema
 from app.extensions import db
+from app.blueprints.routes import get_datasets
 import requests
 import logging
-# from app.tasks import add_together, get_status, process_ids, increment_version, create_database_from_template # incremental_materialization_task
 import numpy as np
 import json
 
@@ -19,43 +19,15 @@ def celery_test():
     results = [test_celery.delay(1, 2) for i in range(tasks)]
     return f"Creating {tasks} tasks"
 
-@api.route("/metadata/<dataset_name>", methods=('GET', ))
-def get_metadata(dataset_name):
+@api.route("/metadata/<dataset_name>/<version>", methods=('GET', ))
+def get_metadata(dataset_name, version):
     from app.tasks import get_materialization_metadata
-    results = get_materialization_metadata(dataset_name)
-    logging.info("Results are: ", results)
+    results = get_materialization_metadata(dataset_name, version)
+    logging.info(f"Results are: {results}")
     return results
    
-
-@api.route("/max_root_id/<dataset_name>", methods=('GET', ))
-def get_max_root_id(dataset_name):
-    from app.tasks import get_max_root_id
-    results = get_max_root_id(dataset_name)
-    logging.info("Results are: ", results)
-    return results
-
-@api.route("/create_database/<template_dataset_name>/<new_database_name>", methods=('GET', ))
-def create_database(template_dataset_name: str, new_database_name: str):
-    from app.tasks import create_database_from_template
-    task = create_database_from_template.delay(template_dataset_name, new_database_name)
-    return f"Creating database {new_database_name} from template {template_dataset_name}"
-
-@api.route("/status")
-def status():
-    from app.tasks import get_status
-    result = get_status.delay()
-    return jsonify(name='celery', task=str(result.task_id), status=str(result.state))
-
-@api.route("/increment/<dataset_name>", methods=('GET', ))
-def new_version(dataset_name):
-    from app.tasks import increment_version
-    increment_version(dataset_name)
-    return jsonify(name=f'increment version:{dataset_name}')
-
-@api.route("/materialize/<dataset_name>")
-def materialize_annotations(dataset_name):
-    from app.tasks import materialize_annotations
-    # if (dataset_name not in get_datasets()):
-    #     abort(404, "Dataset name not valid")
-    materialize_annotations.apply_async([dataset_name])
-    return jsonify({}), 200
+@api.route("/run/<dataset_name>/<dataset_version>")
+def materialize_annotations(dataset_name, dataset_version):
+    from app.tasks import run_materialization
+    run_materialization(dataset_name, dataset_version)
+    return jsonify({"Dataset Name": dataset_name, "Version":dataset_version}), 200
