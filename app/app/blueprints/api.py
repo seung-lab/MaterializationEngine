f07adaeeedac5d7d0fad2222dc96d63d3cb24d05
Derrick <dlbrittain@gmail.com>
2020-03-18 10:04:40 -0700

    Removed uneeded param from endpoint

--- app/app/blueprints/api.py
+++ app/app/blueprints/api.py
@@ -26,10 +26,10 @@ def get_metadata(dataset_name, version):
     logging.info(f"Results are: {results}")
     return results
    
-@api.route("/run/<dataset_name>/<dataset_version>/<use_latest>/<increment>")
+@api.route("/run/<dataset_name>/<dataset_version>/<use_latest>")
 def materialize_annotations(dataset_name, dataset_version, use_latest, increment):
     from app.tasks import run_materialization
-    run_materialization(dataset_name, dataset_version, use_latest, increment)
+    run_materialization(dataset_name, dataset_version, use_latest)
     return jsonify({"Dataset Name": dataset_name, "Version":dataset_version}), 200
 
 # @api.route("/new/<dataset_name>/<dataset_version>/<use_latest>/<increment>")
