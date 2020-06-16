3a6945e18d3f3a3aaef5e49c700f8904964af3af
dlbrittain <dlbrittain@gmail.com>
2020-06-16 12:48:11 -0700

    Create flask entrypoint file

---
+++ run.py
@@ -0,0 +1,20 @@
+# Run a test server.
+from werkzeug.serving import WSGIRequestHandler
+from materializationengine import create_app, create_celery
+from materializationengine.extensions import celery
+import os
+
+HOME = os.path.expanduser("~")
+
+application = create_app()
+celery = create_celery(application, celery)
+
+if __name__ == "__main__":
+
+    WSGIRequestHandler.protocol_version = "HTTP/1.1"
+
+    application.run(host='0.0.0.0',
+                    port=8000,
+                    debug=True,
+                    threaded=True,
+                    ssl_context='adhoc')
