dc7e5ca31e6d72353b0c2590992a80adda2aac28
dlbrittain <dlbrittain@gmail.com>
2020-06-16 15:00:04 -0700

    generate AnnotationDB connection

--- materializationengine/database.py
+++ materializationengine/database.py
@@ -4,8 +4,9 @@ from dynamicannotationdb.client import AnnotationDBMeta
 
 cache = {}
 
+
 def get_db():
     if 'db' not in cache:
         sql_uri_config = current_app.config['MATERIALIZATION_POSTGRES_URI']
-        # cache["db"] = AnnotationDBMeta(sql_uri=sql_uri_config)
-    return sql_uri_config
+        cache["db"] = AnnotationDBMeta(sql_uri=sql_uri_config)
+    return cache["db"]
