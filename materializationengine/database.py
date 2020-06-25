de69b608884de990d136a528352cf98fccc3a294
dlbrittain <dlbrittain@gmail.com>
2020-06-25 10:12:48 -0700

    use new dynamic annotation interface

--- materializationengine/database.py
+++ materializationengine/database.py
@@ -1,12 +1,14 @@
 from flask import current_app
-from dynamicannotationdb.client import AnnotationDBMeta
-
+from dynamicannotationdb.materialization_client import DynamicMaterializationClient
 
 cache = {}
 
 
-def get_db():
-    if 'db' not in cache:
-        sql_uri_config = current_app.config['MATERIALIZATION_POSTGRES_URI']
-        cache["db"] = AnnotationDBMeta(sql_uri=sql_uri_config)
-    return cache["db"]
+def get_db(aligned_volume) -> DynamicMaterializationClient:
+    if aligned_volume not in cache:
+        sql_uri_config = current_app.config["SQLALCHEMY_DATABASE_URI"]
+        cache[aligned_volume] = DynamicMaterializationClient(
+            aligned_volume=aligned_volume, sql_base_uri=sql_uri_config
+        )
+
+    return cache[aligned_volume]
