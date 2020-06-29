f141940dad7cf55f2d5077fa75977a999509a7eb
dlbrittain <dlbrittain@gmail.com>
2020-06-29 15:00:05 -0700

    update materialization client args

--- materializationengine/database.py
+++ materializationengine/database.py
@@ -7,8 +7,6 @@ cache = {}
 def get_db(aligned_volume) -> DynamicMaterializationClient:
     if aligned_volume not in cache:
         sql_uri_config = current_app.config["SQLALCHEMY_DATABASE_URI"]
-        cache[aligned_volume] = DynamicMaterializationClient(
-            aligned_volume=aligned_volume, sql_base_uri=sql_uri_config
-        )
+        cache[aligned_volume] = DynamicMaterializationClient(aligned_volume, sql_uri_config)
 
     return cache[aligned_volume]
