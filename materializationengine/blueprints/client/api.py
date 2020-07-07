7d0cd02656a4a9d9a7476119a785399095311b49
dlbrittain <dlbrittain@gmail.com>
2020-07-07 14:08:16 -0700

    update materialization endpoints

--- materializationengine/blueprints/client/api.py
+++ materializationengine/blueprints/client/api.py
@@ -10,8 +10,8 @@ from materializationengine.blueprints.client.schemas import (
     Metadata,
     SegmentationTableSchema,
     CreateTableSchema,
-    PutAnnotationSchema,
-    DeleteAnnotationSchema,
+    PostPutAnnotationSchema,
+    GetDeleteAnnotationSchema,
 )
 from middle_auth_client import auth_required, auth_requires_permission
 import logging
@@ -29,11 +29,6 @@ authorizations = {
 client_bp = Namespace("Materialization Client",
                       authorizations=authorizations,
                       description="Materialization Client")
-
-annotation_parser = reqparse.RequestParser()
-annotation_parser.add_argument(
-    "annotation_ids", type=int, action="split", help="list of annotation ids"
-)
 
 
 def check_aligned_volume(aligned_volume):
@@ -79,60 +74,61 @@ class SegmentationTable(Resource):
 class MaterializedAnnotations(Resource):
     @auth_required
     @client_bp.doc("get linked annotations", security="apikey")
-    @client_bp.expect(annotation_parser)
+    @accepts("GetDeleteAnnotationSchema", schema=GetDeleteAnnotationSchema, api=client_bp)
     def get(self, aligned_volume_name: str, table_name: str, **kwargs):
         """ Get annotations and segmentation from list of IDs"""
         check_aligned_volume(aligned_volume_name)
-
-        args = annotation_parser.parse_args()
-
-        annotation_ids = args["annotation_ids"]
+        data = request.parsed_obj
+        ids = data.get("annotation_ids")
+        pcg_table_name = data.get("pcg_table_name")
+        pcg_version = data.get("pcg_version")
 
         db = get_db(aligned_volume_name)
-
-        metadata = db.get_table_metadata(aligned_volume_name, table_name)
-        schema = metadata.get("schema_type")
-
-        annotations = db.get_linked_annotations(table_name, schema, annotation_ids)
+        annotations = db.get_linked_annotations(aligned_volume_name,
+                                                table_name,
+                                                pcg_table_name,
+                                                pcg_version,
+                                                ids)
 
         if annotations is None:
-            msg = f"annotation_id {annotation_ids} not in {table_name}"
+            msg = f"annotation_id {ids} not in {table_name}"
             abort(404, msg)
 
         return annotations, 200
 
     @auth_required
     @client_bp.doc("post linked annotations", security="apikey")
-    @accepts("PutAnnotationSchema", schema=PutAnnotationSchema, api=client_bp)
+    @accepts("PostPutAnnotationSchema", schema=PostPutAnnotationSchema, api=client_bp)
     def post(self, aligned_volume_name: str, table_name: str, **kwargs):
         """ Insert linked annotations """
         check_aligned_volume(aligned_volume_name)
         data = request.parsed_obj
         annotations = data.get("annotations")
-
+        pcg_table_name = data.get("pcg_table_name")
+        pcg_version = data.get("pcg_version")
         db = get_db(aligned_volume_name)
-
-        metadata = db.get_table_metadata(aligned_volume_name, table_name)
-        schema = metadata.get("schema_type")
-
-        if schema:
-            try:
-                db.insert_linked_annotations(table_name, schema, annotations)
-            except Exception as error:
-                logging.error(f"INSERT FAILED {annotations}")
-                abort(404, error)
+        try:
+            db.insert_linked_annotations(aligned_volume_name,
+                                         table_name,
+                                         pcg_table_name,
+                                         pcg_version,
+                                         annotations)
+        except Exception as error:
+            logging.error(f"INSERT FAILED {annotations}")
+            abort(404, error)
 
         return f"Inserted {len(annotations)} annotations", 200
 
     @auth_required
     @client_bp.doc("update linked annotations", security="apikey")
-    @accepts("PutAnnotationSchema", schema=PutAnnotationSchema, api=client_bp)
+    @accepts("PostPutAnnotationSchema", schema=PostPutAnnotationSchema, api=client_bp)
     def put(self, aligned_volume_name: str, table_name: str, **kwargs):
         """ Update linked annotations """
         check_aligned_volume(aligned_volume_name)
         data = request.parsed_obj
         annotations = data.get("annotations")
-
+        pcg_table_name = data.get("pcg_table_name")
+        pcg_version = data.get("pcg_version")
         db = get_db(aligned_volume_name)
 
         metadata = db.get_table_metadata(aligned_volume_name, table_name)
@@ -140,25 +136,31 @@ class MaterializedAnnotations(Resource):
 
         if schema:
             for annotation in annotations:
-                anno_id = annotation.pop("id")
-                db.update_linked_annotations(table_name, schema, anno_id, annotation)
+                db.update_linked_annotations(table_name,
+                                             pcg_table_name,
+                                             pcg_version,
+                                             annotations)
 
         return f"Updated {len(data)} annotations", 200
 
     @auth_required
     @client_bp.doc("delete linked annotations", security="apikey")
-    @accepts("DeleteAnnotationSchema", schema=DeleteAnnotationSchema, api=client_bp)
+    @accepts("GetDeleteAnnotationSchema", schema=GetDeleteAnnotationSchema, api=client_bp)
     def delete(self, aligned_volume_name: str, table_name: str, **kwargs):
         """ Delete linked annotations """
         check_aligned_volume(aligned_volume_name)
         data = request.parsed_obj
 
         ids = data.get("annotation_ids")
-
+        pcg_table_name = data.get("pcg_table_name")
+        pcg_version = data.get("pcg_version")
         db = get_db(aligned_volume_name)
 
         for anno_id in ids:
-            ann = db.delete_annotation(table_name, anno_id)
+            ann = db.delete_linked_annotation(table_name,
+                                              pcg_table_name,
+                                              pcg_version,
+                                              ids)
 
         if ann is None:
             msg = f"annotation_id {ids} not in {table_name}"
