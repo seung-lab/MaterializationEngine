eb86721e51d7d49b87916e2aa1f53c939fa5b60a
dlbrittain <dlbrittain@gmail.com>
2020-07-14 17:54:53 -0700

    add errors

--- materializationengine/errors.py
+++ materializationengine/errors.py
@@ -6,3 +6,15 @@ class MaterializationEngineException(Exception):
 class AlignedVolumeNotFoundException(MaterializationEngineException):
     """ error raised when a aligned_volume is not found """
     pass
+
+
+class MaterializeAnnotationException(Exception):
+    pass
+
+
+class RootIDNotFoundException(MaterializeAnnotationException):
+    pass
+
+
+class AnnotationParseFailure(MaterializeAnnotationException):
+    pass
