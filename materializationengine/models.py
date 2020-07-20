0249cddb52716f08a169345bae24075fc9399353
dlbrittain <dlbrittain@gmail.com>
2020-07-20 15:07:32 -0700

    add analysis metadata model

--- materializationengine/models.py
+++ materializationengine/models.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, String, Integer, Boolean, \
-                       DateTime, ForeignKey
+from sqlalchemy import Column, String, Integer, Boolean, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
 
 
 class AnalysisVersion(Base):
-    __tablename__ = 'analysisversion'
+    __tablename__ = "analysisversion"
     id = Column(Integer, primary_key=True)
     dataset = Column(String(100), nullable=False)
     version = Column(Integer, nullable=False)
@@ -18,10 +18,21 @@ class AnalysisVersion(Base):
 
 
 class AnalysisTable(Base):
-    __tablename__ = 'analysistables'
+    __tablename__ = "analysistables"
+    id = Column(Integer, primary_key=True)
+    aligned_volume = Column(String(100), nullable=False)
+    schema = Column(String(100), nullable=False)
+    table_id = Column(String(100), nullable=False)
+    valid = Column(Boolean)
+    created = Column(DateTime, nullable=False)
+    analysisversion_id = Column(Integer, ForeignKey("analysisversion.id"))
+    analysisversion = relationship("AnalysisVersion")
+
+
+class AnalysisMetadata(Base):
+    __tablename__ = "analysismetadata"
     id = Column(Integer, primary_key=True)
     schema = Column(String(100), nullable=False)
-    tablename = Column(String(100), nullable=False)
+    table_id = Column(String(100), nullable=False)
     valid = Column(Boolean)
-    analysisversion_id = Column(Integer, ForeignKey('analysisversion.id'))
-    analysisversion = relationship('AnalysisVersion')
+    created = Column(DateTime, nullable=False)
