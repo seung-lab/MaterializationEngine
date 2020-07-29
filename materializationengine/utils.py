4387d15a43fcc6fd6ee21f372a7d070be7b5b956
dlbrittain <dlbrittain@gmail.com>
2020-07-29 13:45:09 -0700

    add table_id util methods

--- materializationengine/utils.py
+++ materializationengine/utils.py
@@ -7,3 +7,13 @@ def get_app_base_path():
 
 def get_instance_folder_path():
     return os.path.join(get_app_base_path(), 'instance')
+
+
+def make_root_id_column_name(column_name: str):
+    pos = column_name.split("_")[0]
+    pos_type = column_name.split("_")[1]
+    return f"{pos}_{pos_type}_root_id"
+
+
+def build_materailized_table_id(aligned_volume: str, table_name: str) -> str:
+    return f"mat__{aligned_volume}__{table_name}"
