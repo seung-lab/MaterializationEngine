86075782df21c9466d51eb6c5bcd7643bd7c75f9
Derrick <dlbrittain@gmail.com>
2020-09-29 11:13:32 -0700

    rename celery instance

--- materializationengine/celery_worker.py
+++ materializationengine/celery_worker.py
@@ -1,12 +1,11 @@
 from materializationengine import create_app, create_celery
 from celery import Celery
 
-celery = Celery(include=[
+celery_app = Celery(include=[
     'materializationengine.workflows.live_materialization',
-    'materializationengine.workflows.flat_materialization',
+    'materializationengine.workflows.versioned_materialization',
     'materializationengine.shared_tasks',
-
     ])
 
 app = create_app()
-celery = create_celery(app, celery)
+celery = create_celery(app, celery_app)
