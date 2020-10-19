f074b6632d9e5b5020023e90e16e5dfd8582b038
Derrick <dlbrittain@gmail.com>
2020-10-19 14:07:18 -0700

    update bulk upload tasks

--- materializationengine/workflows/bulk_upload.py
+++ materializationengine/workflows/bulk_upload.py
@@ -38,29 +38,22 @@ SQL_URI_CONFIG = current_app.config["SQLALCHEMY_DATABASE_URI"]
 
 def bulk_upload(bulk_upload_params: dict):
     bulk_upload_params.update({
-        "project_path": "gs://allen-minnie-phase3",
+        "project": "gs://allen-minnie-phase3",
         "file_path": "minniephase3-synapses",
         "schema": "synapse"
     })
-    get_file_data = chain(
-        get_file_info.s(bulk_upload_params))
+    get_file_data = get_file_info.s(bulk_upload_params)
     file_results = get_file_data.apply_async()
     bulk_file_info = file_results.get()
+    
     result = create_chunks.s(bulk_file_info[0]).delay()
     bulk_upload_chunks = result.get()
     
-    create_tables.si(bulk_upload_params).delay()
+    create_tables.si(bulk_file_info[0]).delay()
 
-    for chunk in bulk_upload_chunks:
-            bulk_upload_workflow = chain(
-                chord([
-                    chain(
-                        gcs_read_npy_chunk.s(file_metadata, chunk),
-                        parse_data.s(file_metadata)
-                    ) for file_metadata in bulk_file_info],
-                    format_data.s(bulk_upload_params)),  # return here is required for chords
-                upload_data.si(bulk_upload_params)) # final task which will process a return status/timing etc...
-            bulk_upload_workflow.apply_async()
+    bulk_upload_workflow = group(bulk_upload_task.s(
+        bulk_file_info, chunk) for chunk in bulk_upload_chunks)
+    bulk_upload_workflow.apply_async()
 
 COLUMNS = {
     "cleft_ids": "id",
@@ -71,47 +64,53 @@ COLUMNS = {
     "sv_ids_ordered": ["pre_pt_supervoxel_id", "post_pt_supervoxel_id"],
     "root_ids_ordered": ["pre_pt_root_id", "post_pt_root_id"],        
 }
-    
+
 
 @celery.task(name="process:get_file_info",
              bind=True,
              autoretry_for=(Exception,),
              max_retries=3)  
 def get_file_info(self, bulk_upload_params: dict) -> dict:
-    project_path = bulk_upload_params["project_path"]
+    project_path = bulk_upload_params["project"]
     file_path = bulk_upload_params["file_path"]
     fs = gcsfs.GCSFileSystem(project=project_path)
     npy_files = fs.ls(f"{project_path}/{file_path}")
     npy_files.pop(0)
-    bulk_upload_params = []
+    bulk_file_info = []
     try:
         for npy_file in npy_files:
             with fs.open(npy_file, 'rb') as fhandle:
                 major, minor = np.lib.format.read_magic(fhandle)
                 shape, fortran, dtype = np.lib.format.read_array_header_1_0(fhandle)
-                file_metadata = {
-                    'project': project_path,
+                file_info = {
                     'filename': npy_file,
+                    'project': bulk_upload_params["project"],
+                    'file_path': bulk_upload_params["file_path"],
+                    'schema': bulk_upload_params["schema"],
+                    'description': bulk_upload_params['description'],
+                    'annotation_table_name': bulk_upload_params["annotation_table_name"],
+                    'aligned_volume': bulk_upload_params["aligned_volume"]["name"],
+                    'pcg_table_name': bulk_upload_params["segmentation_source"],
                     'num_rows': int(shape[0]),
                     'data_type': npy_file.split("/")[-1].split('.')[0],
                     'fortran': fortran,
                 }
-                bulk_upload_params.append(file_metadata.copy())
+                 
+                bulk_file_info.append(file_info.copy())
     except Exception as e:
         raise self.retry(exc=e, countdown=3)
 
-    return bulk_upload_params
+    return bulk_file_info
 
 @celery.task(name="process:create_chunks", bind=True)
 def create_chunks(self, bulk_upload_info: dict) -> List:   
     num_rows = bulk_upload_info['num_rows']
-    chunk_size = bulk_upload_info.get('chunk_size', 100_000)
-    
+    chunk_size = bulk_upload_info.get('chunk_size', 1000)
+    chunks = []
     if chunk_size <= 1:
         raise ValueError(f'Chunk size of {chunk_size}, must be larger than 1.')
-    chunks = []
     for chunk_start in range(0, num_rows, chunk_size):        
-        chunk_end = chunk_start + chunk_size - 1
+        chunk_end = chunk_start + chunk_size
         if chunk_end > num_rows:
             chunk_end = num_rows
         chunks.append([chunk_start, chunk_end - chunk_start])
@@ -123,10 +122,8 @@ def create_chunks(self, bulk_upload_info: dict) -> List:
              max_retries=3)       
 def create_tables(self, bulk_upload_params: dict):
     table_name = bulk_upload_params["annotation_table_name"]
-    aligned_volume = bulk_upload_params["aligned_volume"]["name"]
-    
-    AnnotationModel = create_annotation_model(bulk_upload_params)
-    SegmentationModel = create_segmentation_model(bulk_upload_params)
+    aligned_volume = bulk_upload_params["aligned_volume"]
+    pcg_table_name = bulk_upload_params['pcg_table_name']
 
     session = sqlalchemy_cache.get(aligned_volume)
     engine = sqlalchemy_cache.engine
@@ -134,32 +131,34 @@ def create_tables(self, bulk_upload_params: dict):
     creation_time = datetime.datetime.utcnow()
 
     if not session.query(AnnoMetadata).filter(AnnoMetadata.table_name==table_name).scalar():
+        AnnotationModel = create_annotation_model(bulk_upload_params)
         AnnotationModel.__table__.create(bind=engine, checkfirst=True)
         anno_metadata_dict = {
                 'table_name': table_name,
                 'schema_type': bulk_upload_params.get('schema'),
                 'valid': True,
                 'created': creation_time,
-                'user_id': 'test',
-                'description': 'Test upload',
+                'user_id': bulk_upload_params.get('user_id', 'foo@bar.com'),
+                'description': bulk_upload_params['description'],
                 'reference_table': bulk_upload_params.get('reference_table'),
                 'flat_segmentation_source': bulk_upload_params.get('flat_segmentation_source')
             }
         anno_metadata = AnnoMetadata(**anno_metadata_dict)
 
     if not session.query(SegmentationMetadata).filter(SegmentationMetadata.table_name==table_name).scalar():
+        SegmentationModel = create_segmentation_model(bulk_upload_params)
         SegmentationModel.__table__.create(bind=engine, checkfirst=True)
         seg_metadata_dict = {
-            'annotation_table': bulk_upload_params.get('annotation_table_name'),
+            'annotation_table': table_name,
             'schema_type': bulk_upload_params.get('schema'),
-            'table_name': table_name,
+            'table_name': f"{table_name}__{pcg_table_name}",
             'valid': True,
             'created': creation_time,
-            'pcg_table_name': bulk_upload_params.get('pcg_table_name')
+            'pcg_table_name': pcg_table_name
         }
 
         seg_metadata = SegmentationMetadata(**seg_metadata_dict)
-
+    
         try:
             session.add(anno_metadata)
             session.flush()
@@ -173,11 +172,29 @@ def create_tables(self, bulk_upload_params: dict):
         session.close()
         
 
-@celery.task(name="process:gcs_read_npy_chunk",
+@celery.task(name="process:bulk_upload_task",
              bind=True,
              autoretry_for=(Exception,),
-             max_retries=3)       
-def gcs_read_npy_chunk(self, bulk_upload_info: dict, chunk: List):
+             max_retries=3,
+             ignore_results=True,
+             store_errors_even_if_ignored=True)  
+def bulk_upload_task(self, bulk_upload_info: dict, chunk: List):
+    try:
+        file_data = []
+        for file_metadata in bulk_upload_info:
+            celery_logger.info(file_metadata)
+
+            data = gcs_read_npy_chunk(file_metadata, chunk)
+            parsed_data = parse_data(data, file_metadata)
+            file_data.append(parsed_data)
+        
+        formatted_data = format_data(file_data, file_metadata)
+        upload_data(formatted_data, file_metadata)
+    except Exception as e:
+        celery_logger.error(e)
+        raise self.retry(exc=e, countdown=3)
+ 
+def gcs_read_npy_chunk(bulk_upload_info: dict, chunk: List):
     filename = bulk_upload_info['filename']
     project = bulk_upload_info['project']
     start_row = chunk[0]
@@ -186,49 +203,47 @@ def gcs_read_npy_chunk(self, bulk_upload_info: dict, chunk: List):
         raise ValueError()
         
     fs = gcsfs.GCSFileSystem(project=project)
-    try:
-        with fs.open(filename, 'rb') as fhandle:
-            major, minor = np.lib.format.read_magic(fhandle)
-            shape, fortran, dtype = np.lib.format.read_array_header_1_0(fhandle)
-            offset = fhandle.tell()
+    # try:
+    with fs.open(filename, 'rb') as fhandle:
+        major, minor = np.lib.format.read_magic(fhandle)
+        shape, fortran, dtype = np.lib.format.read_array_header_1_0(fhandle)
+        offset = fhandle.tell()
 
-            try:
-                col_shape = shape[1]
-            except IndexError:
-                col_shape = 1      
+        try:
+            col_shape = shape[1]
+        except IndexError:
+            col_shape = 1      
+        
+        if start_row > shape[0]:
+            raise ValueError()
+        if start_row + num_rows > shape[0]:
+            raise ValueError()
             
-            if start_row > shape[0]:
-                raise ValueError()
-            if start_row + num_rows > shape[0]:
-                raise ValueError()
-                
-            total_size = np.prod(shape[:])
-            row_size = int(np.prod(shape[1:]))
-            
-            length = num_rows * dtype.itemsize
-            start_byte = start_row * dtype.itemsize
-            index_row_byte = (total_size // row_size) * dtype.itemsize
-            if fortran:
-                data_bytes = [(start_byte + (index_row_byte * i)) for i in range(0, col_shape)]
+        total_size = np.prod(shape[:])
+        row_size = int(np.prod(shape[1:]))
+        
+        length = num_rows * dtype.itemsize
+        start_byte = start_row * dtype.itemsize
+        index_row_byte = (total_size // row_size) * dtype.itemsize
+        if fortran:
+            data_bytes = [(start_byte + (index_row_byte * i)) for i in range(0, col_shape)]
+        else:
+            fhandle.seek(start_byte)
+            data_bytes = [start_byte]
+
+        array = np.zeros([num_rows, row_size], dtype=dtype)
+        
+        for i, index in enumerate(data_bytes):
+            data = fs.read_block(filename, offset=index+offset, length=length)
+            if col_shape == 1:
+                array = np.frombuffer(data, dtype=dtype)
             else:
-                data_bytes = [start_row * row_size * i for i in range(0, col_shape)]
-
-            array = np.zeros([num_rows, row_size], dtype=dtype)
-            
-            for i, index in enumerate(data_bytes):
-                data = fs.read_block(filename, offset=index+offset, length=length)
-                if col_shape == 1:
-                    array = np.frombuffer(data, dtype=dtype)
-                else:
-                    array[:,i] = np.frombuffer(data, dtype=dtype)
-    except Exception as e:
-        raise self.retry(exc=e, countdown=3)
+                array[:,i] = np.frombuffer(data, dtype=dtype)
     return array.tolist()
 
 
-@celery.task(name="process:parse_data", bind=True)       
-def parse_data(self, data: List, bulk_upload_info: dict):
-    data_type = bulk_upload_info["data_type"]
+def parse_data(data: List, bulk_upload_info: dict):
+    data_type = bulk_upload_info['data_type']
     if data_type in COLUMNS:
         data_columns = COLUMNS[data_type]
        
@@ -240,16 +255,15 @@ def parse_data(self, data: List, bulk_upload_info: dict):
     return formatted_data.to_dict('records')
 
 
-@celery.task(name="process:format_data", bind=True)       
-def format_data(self, data: List, bulk_upload_info: dict):
-    schema = bulk_upload_info["schema"]
+def format_data(data: List, bulk_upload_info: dict):
+    schema = bulk_upload_info['schema']
     
     base_df = pd.DataFrame(data[0])
     for data in data[1:]:
         temp_df = pd.DataFrame(data)
         base_df = pd.concat([base_df, temp_df], axis=1)
 
-    records = base_df.to_dict("records")
+    records = base_df.to_dict('records')
     schema = get_schema(schema)
     FlattendSchema = create_flattened_schema(schema)
 
@@ -280,19 +294,15 @@ def split_annotation_data(serialized_data, schema):
         split_data.append(matched_data)
     return split_data
 
-@celery.task(name="process:upload_data",
-             autoretry_for=(Exception,),
-             max_retries=3,
-             bind=True)       
-def upload_data(self, data: List, bulk_upload_info: dict):
+def upload_data(data: List, bulk_upload_info: dict):
 
-    aligned_volume = bulk_upload_info["aligned_volume"]["name"]
-
-    model_data = {{
-        "annotation_table_name": bulk_upload_info["annotation_table_name"],
-        "schema": bulk_upload_info["schema"],
-        "pcg_table_name": bulk_upload_info["pcg_table_name"],
-    }}
+    aligned_volume = bulk_upload_info["aligned_volume"]
+    celery_logger.info(f"............................METADATA {bulk_upload_info}")
+    model_data = {
+        "annotation_table_name": bulk_upload_info['annotation_table_name'],
+        "schema": bulk_upload_info['schema'],
+        "pcg_table_name": bulk_upload_info['pcg_table_name'],
+    }
 
     AnnotationModel = create_annotation_model(model_data)
     SegmentationModel = create_segmentation_model(model_data)
@@ -307,7 +317,6 @@ def upload_data(self, data: List, bulk_upload_info: dict):
     except Exception as e:
         celery_logger.error(f"ERROR: {e}")
         session.rollback()
-        raise self.retry(exc=e, countdown=3)
     finally:
         session.close()
     
