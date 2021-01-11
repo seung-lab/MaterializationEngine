208db459a95eb35b61179ecd1f4f6ccd7dde16db
Derrick <dlbrittain@gmail.com>
2021-01-11 08:59:22 -0800

    update index mangement

--- materializationengine/index_manager.py
+++ materializationengine/index_manager.py
@@ -15,12 +15,12 @@ class IndexCache:
     def index_maps(self, empty_dict={}):
         self._index_maps = empty_dict  
     
-    def get_table_indexes(self, table_name: str, engine):
+    def get_table_indices(self, table_name: str, engine):
         if table_name not in self._index_maps:
             insp = reflection.Inspector.from_engine(engine)
             try:
                 pk_columns = insp.get_pk_constraint(table_name)
-                indexed_columns = insp.get_indexes(table_name)
+                indexed_columns = insp.get_indices(table_name)
                 foreign_keys = insp.get_foreign_keys(table_name)
             except Exception as e:
                 print(f"No table named '{table_name}', error: {e}")
@@ -40,16 +40,16 @@ class IndexCache:
                 else:
                     return None
             if indexed_columns:
-                indexes = []
+                indices = []
                 for index in indexed_columns:
                     indx_map = {
                         'index_name': index.get("name"),
                         'column_name': index["column_names"][0],
                     }
-                    indexes.append(indx_map.copy())
+                    indices.append(indx_map.copy())
 
                 index_map.update({
-                    'indexes': indexes
+                    'indices': indices
                 })
             if foreign_keys:
                 for fk in foreign_keys:
@@ -69,7 +69,7 @@ class IndexCache:
     def get_index_from_model(self, model):
         model = model.__table__
         index_map = {}
-        indexes = []
+        indices = []
         for column in model.columns:    
             if column.primary_key:
                 pk = {
@@ -84,27 +84,27 @@ class IndexCache:
                     'index_name': f"ix_{model.name}_{column.name}",
                     'column_name': column.name
                 }
-                indexes.append(indx_map)
+                indices.append(indx_map)
             if isinstance(column.type, Geometry): 
                 spatial_index_map = {
                     'index_name': f"idx_{model.name}_{column.name}",
                     'column_name': column.name
                 }
-                indexes.append(spatial_index_map)        
+                indices.append(spatial_index_map)        
 
         index_map.update({
-            'indexes': indexes
+            'indices': indices
         })
 
         self._index_maps[model.name] = index_map
         return self._index_maps[model.name]
     
-    def drop_table_indexes(self, table_name: str, engine):
-        indexes = self.get_table_indexes(table_name, engine)
-        if not indexes:
-            return f"No indexes on '{table_name}' found."
+    def drop_table_indices(self, table_name: str, engine):
+        indices = self.get_table_indices(table_name, engine)
+        if not indices:
+            return f"No indices on '{table_name}' found."
         command = f"ALTER TABLE {table_name}"
-        for index_type, index_columns in indexes.items():
+        for index_type, index_columns in indices.items():
             if index_type == 'foreign_key':
                 fk_list = [index_columns['foreign_key']
                            for i in index_columns]
@@ -113,7 +113,7 @@ class IndexCache:
             if index_type == 'primary_key':
                 drop_pk = f"DROP CONSTRAINT {index_columns['primary_key_name']}"
                 command = f"{command} {drop_pk};"
-            if index_type == 'indexes':
+            if index_type == 'indices':
                 index_list = [col['index_name'] for col in index_columns]
                 drop_index = f"DROP INDEX {', '.join(index_list)}"
                 command = f"{command} {drop_index}"
@@ -124,20 +124,20 @@ class IndexCache:
             raise(e)
         return True
 
-    def add_indexes(self, table_name, model, engine, is_flat=True):
+    def add_indices(self, table_name, model, engine, is_flat=True):
         if table_name not in self._index_maps:
-            indexes = self.get_index_from_model(model)
+            indices = self.get_index_from_model(model)
         else:
-            indexes = self._index_maps[table_name]
+            indices = self._index_maps[table_name]
             
-        for index_type, index_columns in indexes.items():
+        for index_type, index_columns in indices.items():
             if index_type == 'primary_key':
                 pk_col_name = index_columns['column_name']
 
                 connection = engine.connect()
                 connection.execute(
                     f'ALTER TABLE {table_name} add primary key({pk_col_name})')
-            if index_type == 'indexes':
+            if index_type == 'indices':
                 for col in index_columns:
                     index_name = col['index_name']
                     column_name = col['column_name']
