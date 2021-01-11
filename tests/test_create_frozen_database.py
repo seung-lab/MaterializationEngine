166913da926f4759f8e84795f189b570e641bf8c
Derrick <dlbrittain@gmail.com>
2021-01-11 09:02:50 -0800

    update tests

---
+++ tests/test_create_frozen_database.py
@@ -0,0 +1,50 @@
+def test_create_versioned_materialization_workflow():
+    assert False
+
+
+def test_create_new_version():
+    assert False
+
+
+def test_create_analysis_database():
+    assert False
+
+
+def test_create_analysis_tables():
+    assert False
+
+
+def test_insert_annotation_data():
+    assert False
+
+
+def test_copy_data_from_live_table():
+    assert False
+
+
+def test_insert_chunked_data():
+    assert False
+
+
+def test_update_analysis_metadata():
+    assert False
+
+
+def test_check_tables():
+    assert False
+
+
+def test_create_analysis_sql_uri():
+    assert False
+
+
+def test_get_analysis_table():
+    assert False
+
+
+def test_drop_indices():
+    assert False
+
+
+def test_add_indices():
+    assert False
---
+++ tests/test_shared_tasks.py
@@ -0,0 +1,30 @@
+def test_chunk_supervoxel_ids_task():
+    assert False
+
+
+def test_fin():
+    assert False
+
+
+def test_final_task():
+    assert False
+
+
+def test_get_materialization_info():
+    assert False
+
+
+def test_collect_data():
+    assert False
+
+
+def test_query_id_range():
+    assert False
+
+
+def test_chunk_ids():
+    assert False
+
+
+def test_update_metadata():
+    assert False
