db5f3b7c253b37130a0b113cb491123446048252
Derrick <dlbrittain@gmail.com>
2021-01-21 12:30:39 -0800

    update remove_expired_databases task

--- materializationengine/workflows/periodic_database_removal.py
+++ materializationengine/workflows/periodic_database_removal.py
@@ -8,10 +8,12 @@ from celery.utils.log import get_task_logger
 from flask import current_app
 from materializationengine.database import create_session
 from materializationengine.info_client import get_aligned_volumes
+from materializationengine.celery_worker import celery
 from materializationengine.models import AnalysisVersion
 from psycopg2 import sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from sqlalchemy import create_engine
+from sqlalchemy.engine.url import make_url
 
 celery_logger = get_task_logger(__name__)
 
@@ -31,42 +33,45 @@ def get_aligned_volumes_databases():
         set(aligned_volumes).intersection(databases))
     return aligned_volume_databases
 
-
-def remove_expired_databases(aligned_volume_databases) -> None:
+@celery.task(name="process:remove_expired_databases", acks_late=True, bind=True)
+def remove_expired_databases(self, aligned_volume_databases) -> None:
     """
     Remove expired database from time this method is called.
     """
-    current_time = datetime.utcnow()
+    current_time = datetime.datetime.utcnow()
+    dropped_dbs = []                            
 
     for aligned_volume in aligned_volume_databases:
-        sql_uri = f"postgres://postgres:materialize@localhost:5432/{aligned_volume}"
+        sql_base_uri = SQL_URI_CONFIG.rpartition("/")[0]
+        sql_uri = make_url(f"{sql_base_uri}/{aligned_volume}")
         session, engine = create_session(sql_uri)
 
-        expired_versions = (session.query(AnalysisVersion).
-                            filter(AnalysisVersion.expires_on <= current_time).all())
+        expired_versions = session.query(AnalysisVersion).\
+                            filter(AnalysisVersion.expires_on <= current_time).all()
+        with engine.connect() as conn:
+            conn.execution_options(isolation_level="AUTOCOMMIT")
+            for database in expired_versions:
+                try:
+                    sql = "SELECT 1 FROM pg_database WHERE datname='%s'" % database
+                
+                    result_proxy = conn.execute(sql)
+                    result = result_proxy.scalar()
 
+                    if result:
+                        sql = "DROP DATABASE %s" % database
+                        result_proxy = conn.execute(sql)
+                        
+                        expired_database = session.query(AnalysisVersion).\
+                                    filter(AnalysisVersion.version==database.version).one()
+                        expired_database.valid = False
+                        dropped_dbs.append(expired_database)
+                        celery_logger.info(f"Database '{expired_database}' dropped")
+                except Exception as e:
+                    celery_logger.error(f"ERROR: {e}: {database} does not exist")
+        session.commit()
         session.close()
-        engine.dispose()
-
-        con = psycopg2.connect(dbname='postgres',
-                               user='postgres', host='localhost',
-                               password='materialize')
-
-        con.set_isolation_level(
-            ISOLATION_LEVEL_AUTOCOMMIT)
-
-        cur = con.cursor()
-        for database in expired_versions:
-            try:
-                result = cur.execute(sql.SQL("DROP DATABASE {}").format(
-                    sql.Identifier(str(database)))
-                )
-            except Exception as e:
-                celery_logger.error(f"ERROR: {e}: {database} does not exist")
-        con.close()
-        return True
-
+    return dropped_dbs
 
 if __name__ == "__main__":
-
-    remove_expired_databases(get_aligned_volumes_databases())
+    aligned_volume_databases = get_aligned_volumes_databases()
+    dropped_dbs = remove_expired_databases.s(aligned_volume_databases).apply_async()
