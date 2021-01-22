f2d055d048e2fe45554c85e82238c4a0b1ef2799
Derrick <dlbrittain@gmail.com>
2021-01-22 14:21:59 -0800

    cleanup on config

--- materializationengine/config.py
+++ materializationengine/config.py
@@ -7,20 +7,14 @@ import json
 
 
 class BaseConfig:
+    ENV="base"
     HOME = os.path.expanduser("~")
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    # Statement for enabling the development environment
-    BIGTABLE_CONFIG = {
-        "instance_id": "pychunkedgraph",
-        "amdb_instance_id": "pychunkedgraph",
-        "project_id": "neuromancer-seung-import",
-    }
+
     TESTING = False
     LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
     LOGGING_LOCATION = HOME + "/.materializationengine/bookshelf.log"
     LOGGING_LEVEL = logging.DEBUG
-    CHUNKGRAPH_TABLE_ID = "pinky100_sv16"
-    MATERIALIZATION_POSTGRES_URI = "postgres://postgres:materialize@db:5432/materialize"
     SQLALCHEMY_DATABASE_URI = "postgres://postgres:materialize@db:5432/materialize"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REDIS_HOST = os.environ.get("REDIS_SERVICE_HOST")
@@ -45,19 +39,23 @@ class BaseConfig:
         AUTH_TOKEN = ""
 
 class DevConfig(BaseConfig):
+    ENV="development"
     DEBUG = True
 
 
 class TestConfig(BaseConfig):
+    ENV="testing"
     TESTING = True
     SQLALCHEMY_DATABASE_URI = "sqlite://"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    REDIS_HOST = os.environ.get("REDIS_SERVICE_HOST")
-    REDIS_PORT = os.environ.get("REDIS_SERVICE_PORT")
+    REDIS_HOST = "redis"
+    REDIS_PORT = 6379
     REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
     CELERY_BROKER_URL = REDIS_URL
+    CELERY_RESULT_BACKEND = REDIS_URL
 
 class ProductionConfig(BaseConfig):
+    ENV="production"
     LOGGING_LEVEL = logging.INFO
     CELERY_BROKER = os.environ.get("REDIS_URL")
     CELERY_RESULT_BACKEND = os.environ.get("REDIS_URL")
