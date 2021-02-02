93a5dafe9abb48d33e66f8d39e1495fd79535b2e
Derrick <dlbrittain@gmail.com>
2021-02-02 14:58:59 -0800

    periodic mat refactor

--- materializationengine/workflows/periodic_materialization.py
+++ materializationengine/workflows/periodic_materialization.py
@@ -6,22 +6,19 @@ import os
 from typing import List
 
 from celery.utils.log import get_task_logger
-from flask import current_app
 from materializationengine.blueprints.materialize.api import get_datastack_info
 from materializationengine.workflows.complete_workflow import \
     run_complete_worflow
-
+from materializationengine.celery_init import celery    
+from materializationengine.utils import get_config_param
 celery_logger = get_task_logger(__name__)
-
-
-SQL_URI_CONFIG = current_app.config["SQLALCHEMY_DATABASE_URI"]
 
 
 def _get_datastacks() -> List:
     raise NotImplementedError
 
-
-def run_periodic_materialzation() -> None:
+@celery.task(name="process:run_periodic_materialzation")
+def run_periodic_materialzation(days_to_expire: int=None) -> None:
     """
     Run complete materialization workflow. Steps are as follows:
     1. Find missing segmentation data in a given datastack and lookup.
@@ -30,21 +27,19 @@ def run_periodic_materialzation() -> None:
     4. Merge annotation and segmentation tables together
     5. Drop non-materializied tables
     """
-    datastacks = json.loads(os.environ['DATASTACKS'])
-    expires_in_n_days = int(os.environ['EXPIRES_IN_N_DAYS'])
+    try:
+        datastacks = json.loads(os.environ['DATASTACKS'])
+    except:
+        datastacks = get_config_param('DATASTACKS')
 
     for datastack in datastacks:
         try:
-            print(f"Start periodic materialziation job for {datastack}")
-            datastack_info = get_datastack_info(datastack)
+            celery_logger.info(f"Start periodic materialziation job for {datastack}")
+            datastack_info = get_datastack_info(datastack)  
             datastack_info['database_expires'] = True     
-            task = run_complete_worflow.s(datastack_info, expires_in_n_days=expires_in_n_days)
+            task = run_complete_worflow.s(datastack_info, days_to_expire=days_to_expire)
             task.apply_async()
         except Exception as e:
+            celery_logger.error(e)
             raise e
     return True
-
-
-if __name__ == "__main__":
-
-    run_workflow = run_periodic_materialzation()
