0e633c83df845e0d242abf40b2d5ddf10f1030a7
Derrick <dlbrittain@gmail.com>
2021-02-16 14:05:21 -0800

    celery status method

---
+++ materializationengine/celery_status.py
@@ -0,0 +1,18 @@
+from materializationengine.celery_worker import celery
+
+
+def get_celery_worker_status():
+    i = celery.control.inspect()
+    availability = i.ping()
+    stats = i.stats()
+    registered_tasks = i.registered()
+    active_tasks = i.active()
+    scheduled_tasks = i.scheduled()
+    result = {
+        'availability': availability,
+        'stats': stats,
+        'registered_tasks': registered_tasks,
+        'active_tasks': active_tasks,
+        'scheduled_tasks': scheduled_tasks
+    }
+    return result
