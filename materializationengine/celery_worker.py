3ccc91308d228cecbb7a8723f58679057ee3e8ea
Derrick <dlbrittain@gmail.com>
2021-03-04 10:31:18 -0800

    periodic result cleanup task

--- materializationengine/celery_worker.py
+++ materializationengine/celery_worker.py
@@ -5,6 +5,7 @@ from celery.schedules import crontab
 from materializationengine.workflows.periodic_database_removal import remove_expired_databases
 from materializationengine.workflows.periodic_materialization import run_periodic_materialzation
 from materializationengine.workflows.dummy_workflow import dummy_arg_task
+from celery.app import builtins
 
 app = create_app()
 celery = create_celery(app, celery)
@@ -21,3 +22,7 @@ def setup_periodic_tasks(sender, **kwargs):
                              run_periodic_materialzation.s(days_to_expire=30), name='Long Term Support Materialized Database (30 days)')
     # Remove (drop) expired databases every night at midnight"
     sender.add_periodic_task(crontab(hour=8, minute=0, day_of_week='*', day_of_month='*', month_of_year='*'), remove_expired_databases.s(delete_threshold=5), name="Remove Expired Databases (Midnight)")
+
+    # remove expired task results in redis broker
+    sender.add_periodic_task(crontab(hour=0, minute=0, day_of_week='*', day_of_month='*', month_of_year='*'), builtins.add_backend_cleanup_task(celery), name='Clean up back end results')
+
