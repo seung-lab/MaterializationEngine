e34d1a0d2f7aa15d111c490eca0865277cb7b430
Derrick <dlbrittain@gmail.com>
2021-03-26 15:57:57 -0700

    fix: marshmallow-sqlalchemy deprecation error https://github.com/marshmallow-code/marshmallow-sqlalchemy/issues/240

--- materializationengine/schemas.py
+++ materializationengine/schemas.py
@@ -1,16 +1,16 @@
 from materializationengine.models import AnalysisTable, AnalysisVersion
 from flask_marshmallow import Marshmallow
-from marshmallow_sqlalchemy import ModelSchema
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 ma = Marshmallow()
 
 
-class AnalysisVersionSchema(ModelSchema):
+class AnalysisVersionSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = AnalysisVersion
+        load_instance = True
 
-
-class AnalysisTableSchema(ModelSchema):
+class AnalysisTableSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = AnalysisTable
-
+        load_instance = True
