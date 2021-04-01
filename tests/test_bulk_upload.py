a274d327eafc3fb3444cd4c9e9168b0b85f2f129
dlbrittain <dlbrittain@gmail.com>
2021-04-01 16:27:34 -0700

    test: use params in test not from fixture

--- tests/test_bulk_upload.py
+++ tests/test_bulk_upload.py
@@ -2,13 +2,26 @@ from materializationengine.workflows.bulk_upload import get_gcs_file_info
 from unittest import mock
 import logging
 import datetime
-
+import time
 
 @mock.patch("materializationengine.workflows.bulk_upload.gcsfs", autospec=True)
-def test_get_gcs_file_info(mock_gcsfs, bulk_upload_metadata):
+def test_get_gcs_file_info(mock_gcsfs):
     mock_gcsfs.return_value.GCSFileSystem.return_value = "/test_data"
     upload_timestamp = datetime.datetime.utcnow()
-    gcs_info = get_gcs_file_info(upload_timestamp, bulk_upload_metadata)
+
+    bulk_upload_params = {
+        "project": "test_data",
+        "file_path": "bulk_upload_data",
+        "column_mapping": {
+            "test": "test",
+            "col2": "test2"
+        },
+        "annotation_table_name": "test_upload_table",
+        "segmentation_source": "test_pcg",
+        "materialized_ts": time.time() # epoch time
+
+    }
+    gcs_info = get_gcs_file_info(upload_timestamp, bulk_upload_params)
     logging.info(gcs_info)
     assert False
 
